d5c2a9b701cbfa3a4500f7c0b0adf736c408722d
gdemahartaputra-hash <gde.maharta.putra@gmail.com>
2026-09-13 21:22:37 +0700

    Membuat sebuah view yang mengambil data dari model education, memasukkannya ke dalam context, dan meneruskannya ke template education baru.

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Education
 
 
 def show_main(request):
@@ -21,3 +21,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Gde Maharta Putra Wicaksana Ridjasa",
+        "education_list": Education.objects.all().order_by('-started_year'),
+    }
+    return render(request, "education.html", context)
